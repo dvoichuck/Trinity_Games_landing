083505aa3a8cf94c58552f39d28b2172b08c5b72
JustXAH <mac@MacBook-Air-mac.local>
2021-05-07 16:41:45 +0300

    Added doesn't working Scrollify

--- script.js
+++ script.js
@@ -31,15 +31,54 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-    $(function() {
-    var sldr = $('.sldr'),
-    sldrContent = sldr.html(),
-    slideWidth = $('.sl_ctr').outerWidth(),
-    slideCount = $('.sldr img').length,
-    prv_b = $('.prv_b'),
-    nxt_b = $('.nxt_b'),
-    sldrInterval = 3300,
-    animateTime = 1000,
-    course = 1,
-    margin = - slideWidth;
-    $('.sldr img:last').clone().prependTo('.sldr');$('.sldr img').eq(1).clone().appendTo('.sldr');$('.sldr').css('margin-left',-slideWidth);function nxt_bSlide(){interval=window.setInterval(animate,sldrInterval)}function animate(){if(margin==-slideCount*slideWidth-slideWidth){sldr.css({'marginLeft':-slideWidth});margin=-slideWidth*2}else if(margin==0&&course==-1){sldr.css({'marginLeft':-slideWidth*slideCount});margin=-slideWidth*slideCount+slideWidth}else{margin=margin-slideWidth*(course)}sldr.animate({'marginLeft':margin},animateTime)}function sldrStop(){window.clearInterval(interval)}prv_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=-1;animate();course=course2});nxt_b.click(function(){if(sldr.is(':animated')){return false}var course2=course;course=1;animate();course=course2});sldr.add(nxt_b).add(prv_b).hover(function(){sldrStop()},nxt_bSlide);nxt_bSlide()});
+
+
+// $(function() {
+//     $.scrollify({
+//         section: ".allBlock",
+//         sectionName:false,
+//         scrollSpeed: 1100,
+//         after: function(i) {
+//                   if(i===2) {
+//                       $.scrollify.setOptions({
+//                           easing: "easeOutExpo"
+//                       });
+//                   }
+//         }
+//     });
+//
+//     $(".scroll,.scroll-btn").click(function(e) {
+//         e.preventDefault();
+//
+//         $.scrollify.next();
+//     });
+//
+//     var hasHovered = false;
+//     $(".coffee").on("mouseenter focus", function() {
+//         if (hasHovered === false) {
+//             ga("send", "event", "Coffee", "hover", "Buy me a coffee");
+//             hasHovered = true;
+//         }
+//     });
+// });
+
+
+//
+$.scrollify({
+    section : "section",
+    sectionName : false,
+    interstitialSection : "",
+    easing: "easeOutExpo",
+    scrollSpeed: 1100,
+    offset : 0,
+    scrollbars: false,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: true,
+    updateHash: true,
+    touchScroll:true,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+});
